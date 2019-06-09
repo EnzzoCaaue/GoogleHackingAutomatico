b39fc7b039cc4335e552fc3c2d905aad0abc6e08
enzerah <EnzzoCaaue@users.noreply.github.com>
2019-06-08 21:44:20 -0300

    Update main.py
    Adicionado uma opçao pra coletar tudo de uma vez.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #########################################
 #       Criado por Ryan Aragão          #
+#       Revisado por Enzo C.            #
 #            14/02/2019                 #
 #  Script para Realizar Google Hacking  #
-#               V2.0                    #
+#               V3.0                    #
 #########################################
 from googlesearch import search
 
@@ -14,9 +15,9 @@ acesslog = f'inurl:access.log {dominio}'
 indexof = f'inurl:“index of” {dominio}'
 robotstxt = f'inurl:robots.txt {dominio}'
 senha = f'filetype:xls senha | filetype:txt intext:senha site:{dominio}'
+tudo = f'intitle:parceria | intitle:contato | intext:@gmail.com | intext:@contato | intext:@hotmail site:{dominio} | inurl:access.log {dominio} | inurl:“index of” {dominio} | inurl:robots.txt {dominio} | filetype:xls senha | filetype:txt intext:senha site:{dominio}'
 
-
-def menu():
+def options():
     banner = '''
     +-------------------------+-------------------------+
     |             Seja bem vindo a GHPP                 |
@@ -32,42 +33,49 @@ def menu():
     print("3 - Tentar encontrar Index OF")
     print("4 - Tentar encontrar ROBOTS")
     print("5 - Tentar roubar senha")
+    print("6 - Tentar tudo")
 
-menu()
+options()
 while True:
-    menuzinho = int(input("Selecione uma opção: "))
-    if menuzinho == 0:
+    menu = int(input("Selecione uma opção: "))
+    if menu == 0:
         print("Até a proxima")
         break
-    elif menuzinho == 1:
+    elif menu == 1:
         with open("resultadoemails.txt", "w") as stream:
             for url in search(email, stop=20):
                 print(url, file=stream)
                 print("Os URLS foram salvos em resultadoemails.txt")
                 print("Caso queira, selecione outra opção")
-    elif menuzinho == 2:
+    elif menu == 2:
         with open("resultadoacesslog.txt", "w") as stream:
             for url in search(acesslog, stop=20):
                 print(url, file=stream)
                 print("Os URLS foram salvos em resultadoacesslog.txt")
                 print("Caso queira, selecione outra opção")
-    elif menuzinho == 3:
+    elif menu == 3:
         with open("resultadoindexof.txt", "w") as stream:
             for url in search(indexof, stop=20):
                 print(url, file=stream)
                 print("Os URLS foram salvos em resultadoindexof.txt")
                 print("Caso queira, selecione outra opção")
-    elif menuzinho == 4:
+    elif menu == 4:
         with open("resultadorobots.txt", "w") as stream:
             for url in search(robotstxt, stop=20):
                 print(url, file=stream)
                 print("Os URLS foram salvos em resultadorobots.txt")
                 print("Caso queira, selecione outra opção")
-    elif menuzinho == 5:
+    elif menu == 5:
         with open("resultadosenhas.txt", "w") as stream:
             for url in search(senha, stop=20):
                 print(url, file=stream)
                 print("Os URLS foram salvos em resultadosenhas.txt")
+                print("Caso queria, selecione outra opção")
+     elif menu == 6:
+        with open("resultadotudo.txt", "w") as stream:
+            for url in search(tudo, stop=20):
+                print(url, file=stream)
+                print("Os URLS foram salvos em resultadotudo.txt")
                 print("Caso queria, selecione outra opção")
     else:
         print("Essa opção não existe, tente novamente!")
